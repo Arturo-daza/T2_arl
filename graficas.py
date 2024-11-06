5c412e1fa3748f11cc5f96e57493bb8e299bc035
tech <tech@gmail.com>
2024-11-05 23:00:25 -0500

    update graphs

--- graficas.py
+++ graficas.py
@@ -2,71 +2,34 @@ import plotly.graph_objects as go
 import numpy as np 
 
 def graficar_t2_con_limite(t2_values, LCynt, title='Gráfico de Control T² de Hotelling'):
-    import numpy as np
-    import plotly.graph_objects as go
-
     fig = go.Figure()
 
-    x_values = np.arange(1, len(t2_values) + 1)
-
-    # Crear una lista de colores basada en si el valor de T² excede LCynt
-    colors = ['#FF073A' if t > LCynt else '#39FF14' for t in t2_values]  # Rojo neón y verde neón
-
-    # Añadir la línea que conecta los puntos (solo para los puntos dentro del límite)
-    fig.add_trace(go.Scattergl(
-        x=x_values,
+    # Añadir los valores de T² a la gráfica
+    fig.add_trace(go.Scatter(
+        x=np.arange(1, len(t2_values) + 1),
         y=t2_values,
-        mode='lines',
-        line=dict(color='#39FF14', width=1),
-        name='Línea de T²',
-        showlegend=False
-    ))
-
-    # Añadir los puntos de T² con colores según la condición
-    fig.add_trace(go.Scattergl(
-        x=x_values,
-        y=t2_values,
-        mode='markers',
-        marker=dict(
-            color=colors,
-            size=6
-        ),
+        mode='lines+markers',
         name='Valores de T²'
     ))
 
-    # Añadir el Límite de Control Superior
+    # Añadir el Límite de Control Superior a la gráfica
     fig.add_trace(go.Scatter(
         x=[1, len(t2_values)],
         y=[LCynt, LCynt],
         mode='lines',
-        line=dict(color='red', dash='dash', width=2),
-        name=f'Límite LCynt = {LCynt}'
+        line=dict(color='red', dash='dash'),
+        name='Límite LCynt'
     ))
-
-    # Configurar el rango del eje x
-    x_min = 1
-    x_max = len(t2_values)
 
     # Configurar el diseño de la gráfica e incluir el rango deslizante en el eje x
     fig.update_layout(
         title=title,
         xaxis_title='Número de Muestra',
         yaxis_title='Valor de T²',
-        template='plotly_dark',
+        template='plotly_white',
         xaxis=dict(
-            range=[x_min, x_max],
-            rangeslider=dict(
-                visible=True,
-                range=[x_min, x_max]
-            ),
+            rangeslider=dict(visible=True),
             type='linear'
-        ),
-        legend=dict(
-            orientation="h",
-            yanchor="bottom",
-            y=1.02,
-            xanchor="center",
-            x=0.5
         )
     )
 
